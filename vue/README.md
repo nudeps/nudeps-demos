7454ae6df380a0674040bfbe70c54bb51dc5116c
Lea Verou <lea@verou.me>
2026-03-04 15:42:06 -0500

    Remove Vue override, no longer needed

--- vue/README.md
+++ vue/README.md
@@ -1,3 +1,1 @@
 # Nudeps Vue Demo
-
-This also showcases how overrides work, as Vue's default export expects a NodeJS environment.
--- vue/nudeps.js
+++
@@ -1,8 +0,0 @@
-export default {
-	overrides: {
-		imports: {
-			// Override is ./node_modules based, nudeps will transform it accordingly
-			vue: './node_modules/vue/dist/vue.esm-browser.js',
-		},
-	},
-}
